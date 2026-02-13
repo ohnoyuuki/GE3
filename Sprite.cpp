bbd539a53506ac45d0a1cfd75a35bf5c2efc15eb
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-02-13 18:59:56 +0900

    t

--- Sprite.cpp
+++ Sprite.cpp
@@ -1,5 +1,54 @@
 #include "Sprite.h"
+#include"SpriteCommon.h"
+#include "DirectXCommon.h"
 
-void Sprite::Initialize()
+
+void Sprite::Initialize(SpriteCommon* spriteCommon)
 {
+	// 引数で受け取ってメンバ変数に記録する
+	this->spriteCommon_ = spriteCommon;
+
+	vertexResource = spriteCommon_->GetDxCommon()->CreateBufferResource(sizeof(VertexData) * 6);
+
+	indexResource = spriteCommon_->GetDxCommon()->CreateBufferResource(sizeof(uint32_t) * 6);
+
+	//リソースの先頭のアドレスから使う
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	//使用するリソースのサイズは頂点６つ分のサイズ
+	vertexBufferView.SizeInBytes = sizeof(VertexData) * 4;
+	//１頂点あたりのサイズ
+	vertexBufferView.StrideInBytes = sizeof(VertexData);
+
+
+	//リソースの先頭のアドレスから使う
+	indexBufferView.BufferLocation = indexResource->GetGPUVirtualAddress();
+	//使用するリソースのサイズはインデックス６つ分のサイズ
+	indexBufferView.SizeInBytes = sizeof(uint32_t) * 6;
+	//インデックスはuint32_tとする
+	indexBufferView.Format = DXGI_FORMAT_R32_UINT;
+
+
+	//書き込むためのアドレスを取得
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	vertexData[0].position = { 0.0f,360.0f,0.0f,1.0f };
+	vertexData[0].texcoord = { 0.0f,1.0f };
+	//vertexData[0].normal = { 0.0f,0.0f,-1.0f };
+
+	vertexData[1].position = { 0.0f,0.0f,0.0f,1.0f };
+	vertexData[1].texcoord = { 0.0f,0.0f };
+	//vertexData[1].normal = { 0.0f,0.0f,-1.0f };
+
+	vertexData[2].position = { 640.0f,360.0f,0.0f,1.0f };
+	vertexData[2].texcoord = { 1.0f,1.0f };
+	//vertexData[2].normal = { 0.0f,0.0f,-1.0f };
+
+	vertexData[3].position = { 640.0f,0.0f,0.0f,1.0f };
+	vertexData[3].texcoord = { 1.0f,0.0f };
+	//vertexData[3].normal = { 0.0f,0.0f,-1.0f };
+
+	indexResource->Map(0, nullptr, reinterpret_cast<void**>(&indexData));
+	indexData[0] = 0; indexData[1] = 1; indexData[2] = 2;
+	indexData[3] = 1; indexData[4] = 3; indexData[5] = 2;
 }
+
+
--- Sprite.h
+++ Sprite.h
@@ -1,8 +1,64 @@
 #pragma once
+#include <cstdint>
+#include <d3d12.h>
+#include <wrl.h>
+
+class SpriteCommon;
 class Sprite
 {
 public: // メンバ関数
     //初期化
-    void Initialize();
+    void Initialize(SpriteCommon* spriteCommon);
+
+    struct Vector4
+    {
+        float x;
+        float y;
+        float z;
+        float w;
+    };
+    struct Vector3
+    {
+        float x;
+        float y;
+        float z;
+    };
+
+    struct Vector2
+    {
+        float x;
+        float y;
+    };
+
+    struct VertexData
+    {
+        Vector4 position;
+        Vector2 texcoord;
+    };
+
+    struct Material
+    {
+        Vector4 color;
+      /*  int32_t enableLighting;
+        float padding[3];
+        Matrix4x4 uvTransform;
+        float shininess;*/
+    };
+
+private:
+
+    SpriteCommon* spriteCommon_ = nullptr;
+
+    Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource;
+
+    //頂点インデックス
+    Microsoft::WRL::ComPtr<ID3D12Resource>  indexResource;
+
+    // バッファリソース内のデータを指すポインタ
+    VertexData* vertexData = nullptr;
+    uint32_t* indexData = nullptr;
+    // バッファリソースの使い道を補足するバッファビュー
+    D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
+    D3D12_INDEX_BUFFER_VIEW indexBufferView;
 };
 
