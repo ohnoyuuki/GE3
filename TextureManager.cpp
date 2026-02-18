838294b9d8b9e04e437bdef2bfdcb1bb928186d5
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-02-18 12:39:36 +0900

    t

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -2,6 +2,9 @@
 #include "DirectXCommon.h"
 #include "StringUtility.h"
 
+using namespace StringUtility;
+
+
 TextureManager* TextureManager::instance = nullptr;
 
 TextureManager* TextureManager::GetInstance()
@@ -26,5 +29,17 @@ void TextureManager::Finalize()
 
 void TextureManager::LoadTexture(const std::string& filePath)
 {
+	//テクスチャファイルを読んでプログラムで扱えるようにする
+	DirectX::ScratchImage image{};
+	std::wstring filePathW = ConvertString(filePath);
+	HRESULT hr = DirectX::LoadFromWICFile(filePathW.c_str(), DirectX::WIC_FLAGS_FORCE_RGB, nullptr, image);
+	assert(SUCCEEDED(hr));
 
+	//ミップマップの作成
+	DirectX::ScratchImage mipImages{};
+	hr = DirectX::GenerateMipMaps(image.GetImages(), image.GetImageCount(), image.GetMetadata(), DirectX::TEX_FILTER_SRGB, 4, mipImages);
+	assert(SUCCEEDED(hr));
+
+	//ミップマップ付きのデータを返す
+	return mipImages;
 }
